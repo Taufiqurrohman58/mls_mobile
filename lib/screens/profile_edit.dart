ba3bde454f020004284dab0cc41741cd557eef10
Taufiqurrohman58 <taufiqurrohman250503@gmail.com>
2025-12-24 22:15:09 +0700

    membuat bagian conten di edit profile di profile screen

--- lib/screens/profile_edit.dart
+++ lib/screens/profile_edit.dart
@@ -1,10 +1,103 @@
 import 'package:flutter/material.dart';
 
-class ProfileEdit extends StatelessWidget {
+class ProfileEdit extends StatefulWidget {
   const ProfileEdit({super.key});
 
   @override
+  State<ProfileEdit> createState() => _ProfileEditState();
+}
+
+class _ProfileEditState extends State<ProfileEdit> {
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _countryController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Edit Profile"));
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 60, left: 25, right: 25),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('nama', style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold, fontFamily: 'SansSerif'),),
+              SizedBox(height: 10),
+              TextField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('nama belakang',style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold, fontFamily: 'SansSerif'),),
+              SizedBox(height: 10),
+              TextField(
+                controller: _lastNameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('E-mail Address',style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold, fontFamily: 'SansSerif'),),
+              SizedBox(height: 10),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Negara',style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold, fontFamily: 'SansSerif'),),
+              SizedBox(height: 10),
+              TextField(
+                controller: _countryController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Deskripsi',style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold, fontFamily: 'SansSerif'),),
+              SizedBox(height: 10),
+              TextField(
+                controller: _descriptionController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
